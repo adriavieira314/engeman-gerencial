be5da73082707ead869acdbbc43c109452e59f47
adriavieira314 <adriavieira314@gmail.com>
2021-12-28 20:30:21 -0400

    server.js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/tela-gerencial'));
+app.use(express.static(__dirname + '/dist/tela-gerencial'));
 
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/tela-gerencial/'}),
-);
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname +'/dist/tela-gerencial/index.html'));
+});
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
