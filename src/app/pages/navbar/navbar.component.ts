e427b94b2414a72076c1c96df7db84db181d4c1b
adriavieira314 <adriavieira314@gmail.com>
2021-12-29 22:30:43 -0400

    sidebar para telas menores que 1000

--- src/app/pages/navbar/navbar.component.ts
+++ src/app/pages/navbar/navbar.component.ts
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  screenWidth: any;
+  isMobile: boolean = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.screenWidth = window.innerWidth;
+    if (this.screenWidth < 1000) {
+      this.isMobile = true;
+    }
+    console.log(this.screenWidth);
   }
 
 }
